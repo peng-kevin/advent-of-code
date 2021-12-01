5b68d5b13722261e00a0f98230566f794a42f1b4
Kevin Peng <kevinpeng02@gmail.com>
2021-11-30 21:51:53 -0800

    Improve Day1 Part2

--- day01/part2.py
+++ day01/part2.py
@@ -1,13 +1,8 @@
 with open("input.txt") as f:
-  lines = [int(x) for x in f.readlines()];
-prev1 = lines[0]
-prev2 = lines[1]
-prev3 = lines[2]
+  lines = [int(x) for x in f.readlines()]
+prev = lines[0]
 sum = 0
-for l in lines[1:-2]:
-  if prev2 + prev3 + l > prev1 + prev2 + prev3:
+for a, b in zip(lines[:-3], lines[3:]):
+  if b > a:
     sum += 1
-  prev1 = prev2
-  prev2 = prev3
-  prev3 = l
 print(sum)
