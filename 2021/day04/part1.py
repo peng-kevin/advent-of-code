b35c73df86429a3dc052082457024e70d1869897
Kevin Peng <kevinpeng02@gmail.com>
2021-12-03 21:25:07 -0800

    Complete Day 4

---
+++ 2021/day04/part1.py
@@ -0,0 +1,44 @@
+import re
+
+inputf = "input.txt"
+
+class Board:
+  def __init__(self, grid):
+    self.grid = grid
+
+  def isWin(self):
+    for row in self.grid:
+      if sum(row) == -5:
+        return True
+    col_t = [sum(x) for x in zip(*self.grid)]
+    for c in col_t:
+      if c == -5:
+        return True
+    return False
+
+  def mark(self, val):
+    self.grid = [[-1 if x == val else x for x in row] for row in self.grid]
+
+  def score(self):
+    scoregrid = [[0 if x == -1 else x for x in row] for row in self.grid]
+    return sum(sum(scoregrid, []))
+
+boards = []
+with open(inputf) as f:
+  draws = [int(x) for x in f.readline().split(",")]
+  boards_str = f.read().split("\n\n")
+  for b in boards_str:
+    rows = b.split("\n")
+    grid = []
+    for r in rows:
+      grid.append([int(x) for x in r.split()])
+    boards.append(Board(grid))
+
+for d in draws:
+  print(d)
+  for b in boards:
+    b.mark(d)
+    if(b.isWin()):
+      print("winner:")
+      print(b.score() * d)
+      exit()
---
+++ 2021/day04/part2.py
@@ -0,0 +1,48 @@
+import re
+
+inputf = "input.txt"
+
+class Board:
+  def __init__(self, grid):
+    self.grid = grid
+    self.disabled = False
+
+  def isWin(self):
+    if(self.disabled):
+      return False
+    for row in self.grid:
+      if sum(row) == -5:
+        return True
+    col_t = [sum(x) for x in zip(*self.grid)]
+    for c in col_t:
+      if c == -5:
+        return True
+    return False
+
+  def mark(self, val):
+    self.grid = [[-1 if x == val else x for x in row] for row in self.grid]
+
+  def score(self):
+    scoregrid = [[0 if x == -1 else x for x in row] for row in self.grid]
+    return sum(sum(scoregrid, []))
+  def disable(self):
+    self.disabled = True
+
+boards = []
+with open(inputf) as f:
+  draws = [int(x) for x in f.readline().split(",")]
+  boards_str = f.read().split("\n\n")
+  for b in boards_str:
+    rows = b.split("\n")
+    grid = []
+    for r in rows:
+      grid.append([int(x) for x in r.split()])
+    boards.append(Board(grid))
+
+for d in draws:
+  for b in boards:
+    b.mark(d)
+    if(b.isWin()):
+      print("winner:")
+      print(b.score() * d)
+      b.disable()
