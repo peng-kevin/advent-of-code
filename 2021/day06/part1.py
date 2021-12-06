f678a4747801f5b59fa9d28fa1e874c27cbd5036
Kevin Peng <kevinpeng02@gmail.com>
2021-12-05 21:10:48 -0800

    Complete 2021 Day 6

---
+++ 2021/day06/part1.py
@@ -0,0 +1,20 @@
+import re
+
+inputf = "input.txt"
+with open(inputf) as f:
+    times = [int(x) for x in re.findall("\d+", f.readline())];
+record = [0] * 9
+
+for t in times:
+    record[t] += 1
+for i in range(80):
+    next = [0] * 9
+    for j in range(1, 9):
+        next[j - 1] = record[j]
+    next[6] += record[0]
+    next[8] += record[0]
+    record = next
+sum = 0
+for i in record:
+    sum = sum + i
+print(sum)
