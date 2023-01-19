835ec3c931d4e997c537200a1889d00adc65f932
Kevin Peng <kevinpeng02@gmail.com>
2023-01-18 22:55:28 -0800

    Add star table
    Because the emoji for a star is ":star:", the markdown tables are too wide
    to comfortably maintain, so I created a script to generate it for using
    a different table that uses "X" instead of ":star:".

---
+++ generate_star_table.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# It is difficult to maintain a table showing the stars collected in Advent of
+# Code because the star emoji (:star:) is so wide and there are so many columns,
+# so my solution is to create a data table which represents stars with "X" and
+# generate a nicely formatted table with this script.
+
+# File containing star data
+star_file = 'star_table_data.md'
+# README file
+readme_file = 'README.md'
+# maximum length of a column
+min_length = len(':star::star:')
+# The lines marking the start and end of the table in the README
+table_start_marker = '[comment]: # (STAR TABLE START)'
+table_end_marker = '[comment]: # (STAR TABLE END)'
+
+# Pads out the columns to line up the formatting and replace "X" with ":star:"
+def process_line(line, pad_char, min_length):
+    result = ''
+    # Because we have a '|' at the beginning and end, the first and last
+    # character are an empty string and line break respectively, so we discard
+    # them
+    data = line.split('|')[1:-1]
+    # Copy the first column untouched because it is the year column
+    result += '|' + data[0] + '|'
+    # For each other entry, pad it min_length with the pad_char
+    for entry in data[1:]:
+        entry = entry.replace('X', ':star:').ljust(min_length, pad_char)
+        result += entry + '|'
+    # Add line break to make the result a proper line
+    result += '\n'
+    return result
+
+# Processes each line to generate the processed table
+def process_table(star_file):
+    result = ''
+    with open(star_file, 'r', encoding='utf-8') as file:
+        for i, line in enumerate(file):
+            if i == 1:
+                # The divider line should be padded with hypens
+                result += process_line(line, '-', min_length)
+            else:
+                result += process_line(line, ' ', min_length)
+    return result
+
+# Inserts the processed_table into the readme between the delimeter
+def inject_table(readme_file, start_marker, end_marker, table):
+    with open(readme_file, 'r+', encoding='utf-8') as file:
+        text = file.read()
+
+        # Get the text before the start_marker
+        partition = text.partition(start_marker)
+        if partition[1] == '':
+            print(f'Error: table start marker "{start_marker}" not found in {readme_file}')
+            return
+        before = partition[0]
+
+        # Get the text after the end_marker
+        partition = text.partition(end_marker)
+        if partition[1] == '':
+            print(f'Error: table end marker "{end_marker}" not found in {readme_file}')
+            return
+        after = partition[2]
+
+        # Insert the table between the markers and write it
+        file.seek(0)
+        file.truncate(0)
+        new_text = before + start_marker + '\n\n' + table + '\n\n' + end_marker + after
+        print(new_text, end='')
+        file.write(new_text)
+
+processed_table = process_table(star_file)
+inject_table(readme_file, table_start_marker, table_end_marker, processed_table)
